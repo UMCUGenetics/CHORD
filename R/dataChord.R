1cb7517c4a47143e029317028280156657c286c8
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-04-05 14:20:04 +0200

    Removed duplicated CHORD.rds file

--- R/dataChord.R
+++ R/dataChord.R
@@ -12,5 +12,4 @@
 #--------- Code used to save the random forest model into the CHORD R package ---------#
 # model_dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/training/models/38_HMF_pkgMutSigs_germLt3-somEq0-lohEq0_greylistIncl_multiclass_snvContext_svContext_allSigsRel_scaleGridSearch_featPreFilt_boruta_newAnn/'
 # CHORD <- readRDS(paste0(model_dir,'/final/rf_out.rds'))$RF
-# saveRDS(CHORD, file='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/data/CHORD.rds')
 # save(CHORD,file='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/data/CHORD.rda')
