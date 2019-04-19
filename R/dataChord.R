6ddf11ed77f0b5a550a77f73d682c5c3aba0407a
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-04-19 10:01:06 +0200

    Updated CHORD to v53. Model now trained with improved training set. From BRCA1/2 class: - Removed samples with MSI - Removed samples where there was still REF read support at the pathogenic mutation site, suggesting that WT BRCA1/2 is still present - Removed 3 samples that have a HR proficient-like phenotype based on t-SNE clustering From the 'none' class: - For LOH+somatic hits, mutation must now be likely benign or lower (previously allowed missense/VUS or lower)

--- R/dataChord.R
+++ R/dataChord.R
@@ -2,14 +2,14 @@
 #'
 #' A random forest predictor of homologous recombination deficiency
 #'
-#' Version 38: 38_HMF_pkgMutSigs_germLt3-somEq0-lohEq0_greylistIncl_multiclass_snvContext_svContext_allSigsRel_scaleGridSearch_featPreFilt_boruta_newAnn
+#' Version 53: 53_snvContext_svContext_allSigsRel_strictSuppEvidence_customBlacklist
 #'
 #' @docType data
 #'
 #' @usage data(CHORD)
 'CHORD'
 
-#--------- Code used to save the random forest model into the CHORD R package ---------#
-# model_dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/training/models/38_HMF_pkgMutSigs_germLt3-somEq0-lohEq0_greylistIncl_multiclass_snvContext_svContext_allSigsRel_scaleGridSearch_featPreFilt_boruta_newAnn/'
+# #--------- Code used to save the random forest model into the CHORD R package ---------#
+# model_dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/training/models4/53_snvContext_svContext_allSigsRel_strictSuppEvidence_customBlacklist/'
 # CHORD <- readRDS(paste0(model_dir,'/final/rf_out.rds'))$RF
 # save(CHORD,file='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/data/CHORD.rda')
