04c92a3f3599c08ab6386a316e7dea7e582e9328
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-06-03 10:32:53 +0200

    Updated CHORD to v60: - Trained with HMF DR010-DR047 data - Training samples put through a post-filtering step (100x recursive cross-validation). BRCA1/2 deficient samples that were HRP < 60% of the time were removed. 'None' samples that were HRD > 40% of the time were removed. - Boruta no longer used in the feature selection steps

--- R/dataChord.R
+++ R/dataChord.R
@@ -2,7 +2,7 @@
 #'
 #' A random forest predictor of homologous recombination deficiency
 #'
-#' Version 53: 53_snvContext_svContext_allSigsRel_strictSuppEvidence_customBlacklist
+#' Version 60: 60_snvContext_svContext_allSigsRel_noSuppEvidence_customBlacklist3_noBoruta
 #'
 #' @docType data
 #'
@@ -10,6 +10,6 @@
 'CHORD'
 
 # #--------- Code used to save the random forest model into the CHORD R package ---------#
-# model_dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/training/models4/53_snvContext_svContext_allSigsRel_strictSuppEvidence_customBlacklist/'
+# model_dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/HMF_DR010_DR047/scripts/chord_training/main/output/60_snvContext_svContext_allSigsRel_noSuppEvidence_customBlacklist3_noBoruta/seed015/'
 # CHORD <- readRDS(paste0(model_dir,'/final/rf_out.rds'))$RF
 # save(CHORD,file='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/data/CHORD.rda')
