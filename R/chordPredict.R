a5eebd9be612208ba88292687af5190fa299abd0
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-11-21 16:04:11 +0100

    Added quality control checks (MSI, indel load, SV load)

--- R/chordPredict.R
+++ R/chordPredict.R
@@ -2,34 +2,35 @@
 #' 
 #' @description A wrapper for predict.randomForest() from the randomForest package
 #'
-#' @param sigs The output of extractSigsChord(), which is a dataframe containing the SNV, indel and
+#' @param features The output of extractSigsChord(), which is a dataframe containing the SNV, indel and
 #' SV context counts.
 #' @param rf.model The random forest model. Defaults to CHORD.
 #' @param hrd.cutoff Samples greater or equal to this cutoff will be marked as HRD in the output
 #' table of chordPredict(). Default is 0.5.
-#' @param show.sigs Show the signature matrix in the output table?
+#' @param show.features Show the mutation context matrix in the output table?
+#' @param verbose Show messages/warnings?
 #'
-#' @return A dataframe containing per sample the probabilities of BRCA1/2 deficiency and HRD, as
-#' well as the input signature values if show.sigs==T
+#' @return A dataframe containing per sample the probabilities of BRCA1-type and BRCA2-type HRD, 
+#' and HRD (= BRCA1-type HRD + BRCA2-type HRD)
 #' @export
 #'
 #' @examples
-#' ## Extract signatures
+#' ## Extract mutation contexts
 #' vcf_dir <- '/path_to_vcfs/'
 #' vcf_snv <- paste0(vcf_dir,'SampleX_post_processed_v2.2.vcf.gz')
 #' vcf_indel <- paste0(vcf_dir,'SampleX_post_processed_v2.2.vcf.gz')
 #' vcf_sv <- paste0(vcf_dir,'SampleX_somaticSV_bpi.vcf.gz')
-#' sigs <- extractSigsChord(vcf_snv, vcf_indel, vcf_sv, sample.name='SampleX')
+#' contexts <- extractSigsChord(vcf_snv, vcf_indel, vcf_sv, sample.name='SampleX')
 #' 
 #' ## Predict HRD probability with CHORD
-#' chordPredict(sigs)
+#' chordPredict(contexts)
 
-chordPredict <- function(sigs, rf.model=CHORD, hrd.cutoff=0.5, show.sigs=F){
+chordPredict <- function(features, rf.model=CHORD, hrd.cutoff=0.5, show.features=F, verbose=T){
   
   ## Converts the raw signature counts from extractSigsChord() to features used by CHORD
-  sigs_split <- mutSigExtractor::splitDfRegex(sigs, c(snv='>',indel='[a-z]{3}[.]',sv='[A-Z]{3}'))
-  sigs_processed <- mutSigExtractor::transformContexts(
-    sigs_split,
+  features_split <- mutSigExtractor::splitDfRegex(features, c(snv='>',indel='[a-z]{3}[.]',sv='[A-Z]{3}'))
+  features_processed <- mutSigExtractor::transformContexts(
+    features_split,
     
     ## Simplify indels to types only (mh: flanking microhomology, rep: within repeat regions, none: 
     ## other indels) by ignoring indel length.
@@ -41,24 +42,44 @@ chordPredict <- function(sigs, rf.model=CHORD, hrd.cutoff=0.5, show.sigs=F){
     rel.types = c('snv','indel','sv')
   )
   
-  df <- as.data.frame(predict(rf.model, sigs_processed, type='prob'))
+  ## Prediction
+  df <- as.data.frame(predict(rf.model, features_processed, type='prob'))
   df <- df[,c('none','BRCA1','BRCA2')]
   colnames(df) <- paste0('p_',colnames(df))
-  
-  ## The probability of HRD is the probability of BRCA1 deficiency + BRCA2 deficiency
   df$p_hrd <- df$p_BRCA1 + df$p_BRCA2
-  df$is_hrd <- df$p_hrd >= hrd.cutoff
   
-  if(show.sigs){
-    df <- merge(
-      df,
-      sigs_processed,
-      by='row.names'
+  ## QC
+  df$has_msi <- with(features_split,{
+    rowSums(indel[,grep('rep',colnames(indel)),drop=F]) > 14000
+  })
+  
+  df$low_indel_load <- rowSums(features_split$indel) < 50
+  df$low_sv_load <- rowSums(features_split$sv) < 30
+  
+  df$failed_qc <- with(df,{ has_msi | low_indel_load | low_sv_load })
+  
+  if(verbose & sum(df$failed_qc)>0){
+    message(
+      sum(df$failed_qc),' sample(s) failed QC:\n', 
+      '  ', sum(df$has_msi),' with MSI (>14000 indels within repeats)\n',
+      '  ', sum(df$low_indel_load), ' with <50 indels\n',
+      '  ', sum(df$low_sv_load), ' with <30 SVs'
     )
   }
   
-  df <- cbind(sample=rownames(df),df); rownames(df) <- NULL
-  df <- df[order(df$p_hrd, decreasing=T),]
+  ## Determine if sample is (confident) HRD
+  df$is_hrd <- df$p_hrd >= hrd.cutoff
+  df$is_hrd[ df$failed_qc ] <- NA
+  
+  if(show.features){
+    df <- merge(
+      cbind(sample=rownames(df),df),
+      cbind(sample=rownames(df),features_processed),
+      by='sample'
+    )
+  } else {
+    df <- cbind(sample=rownames(df),df); rownames(df) <- NULL
+  }
   
   return(df)
 }
