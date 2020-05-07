2ee2098a485ba3cfea86c6296320fa59797d4c04
Luan Nguyen <nguyen.n.luan777@gmail.com>
2020-05-07 12:23:34 +0200

    Updated random forest model to CHORD v2

--- R/dataChord.R
+++ R/dataChord.R
@@ -1,15 +1,12 @@
-#' CHORD (Classifier for Homologous Recombination Deficiency)
+#' CHORD (Classifier for Homologous Recombination Deficiency) v2
 #'
 #' A random forest predictor of homologous recombination deficiency
-#'
-#' Version 60: 60_snvContext_svContext_allSigsRel_noSuppEvidence_customBlacklist3_noBoruta
 #'
 #' @docType data
 #'
 #' @usage data(CHORD)
 'CHORD'
 
-# #--------- Code used to save the random forest model into the CHORD R package ---------#
-# model_dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/HMF_DR010_DR047/scripts/chord_training/main/output/60_snvContext_svContext_allSigsRel_noSuppEvidence_customBlacklist3_noBoruta/seed015/'
-# CHORD <- readRDS(paste0(model_dir,'/final/rf_out.rds'))$RF
-# save(CHORD,file='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/data/CHORD.rda')
+#--------- Code used to save the random forest model into the CHORD R package ---------#
+#CHORD <- readRDS('/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORDv2/training/models/2.02_mergedDelMh2-5/rf_model.rds')
+#save(CHORD,file='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/CHORD/data/CHORD.rda')
